ff69ae6b198095e89002531eddaff3701fef9e63
Victor Perin <me@victorperin.ninja>
2019-04-29 16:48:27 -0300

    fix(cli): removing input property, because there's no need to use in the short term

--- src/cli.js
+++ src/cli.js
@@ -4,7 +4,7 @@ const meow = require('meow');
 const oloquinho = require('./index')
 
 // TODO: when tail call optimization is implemented on NodeJS, simplify this.
-const cliMode = async ({ input, flags }) => {
+const cliMode = async ({ flags }) => {
   while(true)
     await oloquinho()
 }
